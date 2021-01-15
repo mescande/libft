18d737be44ad1fa6e78919c58a8f03bd14f376a6
mescande <mescande@student.42.fr>
2021-01-15 15:44:04 +0100

    ft_memrealloc correction

--- srcs/mem/ft_memrealloc.c
+++ srcs/mem/ft_memrealloc.c
@@ -6,7 +6,7 @@
 /*   By: mescande <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/04 12:59:20 by mescande          #+#    #+#             */
-/*   Updated: 2019/06/03 12:41:58 by mescande         ###   ########.fr       */
+/*   Updated: 2021/01/15 15:42:33 by user42           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,13 +16,11 @@
 void	*ft_memrealloc(void *buff, size_t end, size_t init)
 {
 	char	*res;
-	int		i;
 
 	if (!(res = malloc(end)))
 		return (NULL);
-	i = -1;
 	res = ft_memcpy(res, buff, init);
-	ft_bzero(res + i, ((long)(end - init) < 0 ? 0 : end - init));
+	ft_bzero(res + init, ((long)(end - init) < 0 ? 0 : end - init));
 	free(buff);
 	return (res);
 }
